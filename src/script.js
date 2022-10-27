4ca4b621656ddad9778b559c1932097a49077899
Pink-Hat-Hacker <zyv@udel.edu>
2022-10-27 12:49:40 -0400

    minimal changes

--- src/script.js
+++ src/script.js
@@ -1,3 +1,14 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDZb51Z94WZFhVA36pGQ0-ghXyM47dfuxM",
+    authDomain: "craig-says.firebaseapp.com",
+    projectId: "craig-says",
+    storageBucket: "craig-says.appspot.com",
+    messagingSenderId: "1030182600492",
+    appId: "1:1030182600492:web:eb134f469fbd83c871f689",
+    measurementId: "G-XQ9W0VB35J"
+  };
+
 let renderQuote = () => {
     $("#quotes_list").prepend(`
         <div class="card">
