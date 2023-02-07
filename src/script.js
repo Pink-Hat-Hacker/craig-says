d4a777c53f9bd2c71fbbbb7f4626e15918812e85
Pink-Hat-Hacker <zyv@udel.edu>
2023-02-07 13:34:03 -0500

    made guest page render, changed access rules

--- src/script.js
+++ src/script.js
@@ -28,18 +28,6 @@ function login() {
         window.alert(errorMessage + " " + errorCode);
     });
 }
-function signup() {
-    var userName = document.getElementById("user_field").value;
-    var userPass = document.getElementById("pass_field").value;
-    firebase.auth().createUserWithEmailAndPassword(userName, userPass).then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    }).catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        window.alert(errorMessage + " " + errorCode);
-  });
-}
 function logout() {
     firebase.auth().signOut().then(function() {
           //signout success
@@ -127,12 +115,11 @@ let renderPage = () => {
     });
 }
 
-
 let renderLogin = () => {
     $("body").html(`
         <div style="display:flex; justify-content:center;">
             <div class="login">
-                <img src="../assets/EGGMAN.png"/>
+                <img src="assets/EGGMAN.png"/>
                 <h2> CRAIG SAYS LOGIN</h2>
                 <div style="display:flex; justify-content:center;">
                     <div class="login-fields">
@@ -142,17 +129,41 @@ let renderLogin = () => {
                 </div>
                 <button onclick="login()"> Are you Craig? </button>
                 <p> - OTHERWISE - </p>
-                <button onclick="signup()"> Enter as Guest Egg </button>
+                <button onclick="renderGuestPage()"> Enter as Guest Egg </button>
             </div>
         </div>
     `);
 }
 
+function renderGuestPage() {
+    $("body").html(`
+        <div class="header-title">
+            <div class="header-title-content">
+                <img id="eggman" src="assets/EGGMAN.png">
+                <h1>Craig says ... </h1>
+            </div>
+        </div>
+
+        <!-- Quotes -->
+        <div class="quotes">
+            <div id="quotes_list">
+
+            </div>
+        </div>
+    `);
+    //here we can do your bawks or all bawks switch
+    let tweetRef = firebase.database().ref("/posts/");
+    tweetRef.on("child_added", (ss)=>{
+        let tObj = ss.val();
+        renderQuote(tObj, ss.key);
+    });
+}
+
 firebase.auth().onAuthStateChanged(user => {
     if (!user){
-      renderLogin();
+        renderLogin();
     } else {
-      var user_email = firebase.auth().currentUser;
-      renderPage(user, user_email.email);
+        var user_email = firebase.auth().currentUser;
+        renderPage(user, user_email.email);
     }
 })
